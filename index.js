3ffe4b41e1059121cd79412f783ae64247ee33a8
Phong <tangthieuphong888@gmail.com>
2024-09-16 15:35:02 +0700

    complete metadatafile ffc

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var express = require('express');
+const multer = require('multer')
+const path = require('path')
 var cors = require('cors');
 require('dotenv').config()
 
@@ -11,6 +13,21 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+app.post('/api/fileanalyse', upload.single('upfile') ,(req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ error: 'No file uploaded' }); 
+  }
+  const { originalname, mimetype, size } = req.file;
+
+  res.json({
+    name: originalname,
+    type: mimetype,
+    size: size
+  });
+})
 
 
 
